47d3959f807022269de561b6d8c2a4bb725e61ae
Diego Millan <diego.millan@edunext.co>
2019-05-14 14:56:10 -0500

    adding context extender module

---
+++ nau_openedx_extensions/certificates/context_extender.py
@@ -0,0 +1,34 @@
+"""
+Context extender module for edx-platform certificates
+"""
+from django.core.exceptions import ObjectDoesNotExist
+
+from student.forms import get_registration_extension_form
+
+from nau_openedx_extensions.custom_registration_form.models import NauUserExtendedModel
+
+
+def update_cert_context(*args, **kwargs):
+    """
+    Updates certifcates context with custom data for the user
+    """
+    context = kwargs['context']
+    user = kwargs['user']
+    update_context_with_custom_form(user, NauUserExtendedModel, context)
+
+
+def update_context_with_custom_form(user, custom_model, context):
+    """
+    Updates the context in-place with extra user information
+    """
+    custom_form = get_registration_extension_form()
+    try:
+        custom_model_instance = custom_model.objects.get(user=user)
+    except ObjectDoesNotExist:
+        # If a custom model does not exist for the user, just return
+        return
+
+    for field in custom_form.fields.keys():
+        context.update({
+            field: getattr(custom_model_instance, field, "")
+        })
