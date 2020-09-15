54d3d50f0a40badcfb411b846f2db96fac7a90ae
Luis Moreno <luis.moreno@edunext.co>
2020-09-15 10:41:27 -0400

    OEP-18

---
+++ Makefile
@@ -0,0 +1,36 @@
+###############################################
+#
+# eox-tagging commands.
+#
+###############################################
+
+.DEFAULT_GOAL := help
+
+ifdef TOXENV
+TOX := tox -- #to isolate each tox environment if TOXENV is defined
+endif
+
+
+help: ## display this help message
+	@echo "Please use \`make <target>' where <target> is one of"
+	@grep '^[a-zA-Z]' $(MAKEFILE_LIST) | sort | awk -F ':.*?## ' 'NF==2 {printf "\033[36m  %-25s\033[0m %s\n", $$1, $$2}'
+
+clean: ## delete most git-ignored files
+	find . -name '__pycache__' -exec rm -rf {} +
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+
+requirements: ## install environment requirements
+	pip install -r requirements/base.txt
+
+# Define PIP_COMPILE_OPTS=-v to get more information during make upgrade.
+PIP_COMPILE = pip-compile --rebuild --upgrade $(PIP_COMPILE_OPTS)
+
+upgrade: export CUSTOM_COMPILE_COMMAND=make upgrade
+upgrade: ## update the requirements/*.txt files with the latest packages satisfying requirements/*.in
+	pip install -qr requirements/pip-tools.txt
+	# Make sure to compile files after any other files they include!
+	$(PIP_COMPILE) -o requirements/pip-tools.txt requirements/pip-tools.in
+	$(PIP_COMPILE) -o requirements/base.txt requirements/base.in
+
--- setup.py
+++ setup.py
@@ -8,6 +8,40 @@ import re
 from setuptools import setup
 
 
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line)
+        )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, or editable.
+    """
+    # Remove whitespace at the start/end of the line
+    line = line.strip()
+
+    # Skip blank lines, comments, and editable installs
+    return not (
+        line == '' or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+') or
+        line.startswith('-c')
+    )
+
+
 def get_version(*file_paths):
     """Retrieves the version from the main app __init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
@@ -30,7 +64,7 @@ setup(
     url="https://gitlab.fccn.pt/nau/nau-openedx-extensions",
     description="NAU openedX extensions",
     long_description="",
-    install_requires=[],
+    install_requires=load_requirements('requirements/base.in'),
     scripts=[],
     license="AGPL 3.0",
     platforms=["any"],
