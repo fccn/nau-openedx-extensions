48c8f9111623e13ac434fe7283ef3bd5a44d3516
Ivo Branco <ivo.branco@fccn.pt>
2022-01-25 09:37:14 +0000

    Add new NAU custom s3 boto3 storage with a new  storage kwargs.

---
+++ nau_openedx_extensions/storages/backends/s3boto3.py
@@ -0,0 +1,24 @@
+
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class NAUS3Boto3Storage(S3Boto3Storage):
+    """
+    Custom NAU S3 boto3 implementation that allows generate presigned urls to a custom domain.
+    It uses the `nau_custom_endpoint_url` key on storage initialization kwargs to replace the
+    url protocol and domain.
+    """
+
+    nau_custom_endpoint_url = None
+
+    def __init__(self, acl=None, bucket=None, **settings):
+        super().__init__(acl, bucket, **settings)
+
+    def url(self, name, parameters=None, expire=None):
+        url = super().url(name, parameters, expire)
+
+        if self.nau_custom_endpoint_url:
+            import re
+            url_path_and_attributes = re.search("/", url[9:]).start() +9
+            url = self.nau_custom_endpoint_url + url[url_path_and_attributes:]
+
+        return url
