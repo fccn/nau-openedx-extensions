584b110058c3c8e8a69732192fd267ca39c27778
Maria Fernanda Magallanes Zubillaga <maria.magallanes@edunext.co>
2022-08-25 23:39:07 -0500

    feat: add FilterEnrollmentByDomain for FAN-41
    The commits in detail:
    
    feat: add model and admin for DomainsAllowedPerCourse
    
    feat: add FilterEnrollmentByDomain
    
    Revert "feat: add model and admin for DomainsAllowedPerCourse"
    
    This reverts commit 54d8f99015888c13eafdc5fca815ce5fd8d4b1ed.
    
    feat: add wrapper to obtain allowed_domains in other_course_settings
    
    refactor: use "filter_enrollment_by_domain_list" instead domains_allowed
    
    fix: issues with import and issues with obtain filter_enrollment_by_domain_list
    
    feat: allow exception translation and allow match with subdomains

---
+++ nau_openedx_extensions/edxapp_wrapper/course_module.py
@@ -0,0 +1,13 @@
+""" CourseMetadata backend abstraction """
+
+from importlib import import_module
+
+from django.conf import settings
+
+
+def get_other_course_settings(*args, **kwargs):
+    """ Get Other Course Settings """
+    backend_module = settings.NAU_COURSE_MODULE
+    backend = import_module(backend_module)
+
+    return backend.get_other_course_settings(*args, **kwargs)
