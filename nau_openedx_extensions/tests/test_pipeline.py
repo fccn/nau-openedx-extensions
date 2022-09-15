d182e5bc9d1c465c7e3185475eae5478c04e1017
Maria Fernanda Magallanes Zubillaga <maria.magallanes@edunext.co>
2022-09-15 12:23:41 -0500

    refactor: change the domains_allowed in tests

--- nau_openedx_extensions/tests/test_pipeline.py
+++ nau_openedx_extensions/tests/test_pipeline.py
@@ -23,7 +23,7 @@ class FilterEnrollmentByDomainTest(TestCase):
     def test_user_is_allowed_to_enroll_for_allowed_domain(self, get_other_course_settings_mock):
         """Test"""
 
-        get_other_course_settings_mock.return_value = {"domains_allowed": ["example.com"]}
+        get_other_course_settings_mock.return_value = {"filter_enrollment_by_domain_list": ["example.com"]}
 
         response = FilterEnrollmentByDomain.run_filter(self, self.user, self.course_key, self.mode)
 
@@ -41,14 +41,14 @@ class FilterEnrollmentByDomainTest(TestCase):
         response = FilterEnrollmentByDomain.run_filter(self, self.user, self.course_key, self.mode)
 
         get_other_course_settings_mock.assert_called_once_with(self.course_key)
-        course_other_course_settings.get.assert_called_once_with('domains_allowed', [])
+        course_other_course_settings.get.assert_called_once_with('filter_enrollment_by_domain_list', [])
         self.assertEqual(response, {})
 
 
     def test_user_is_not_allowed_to_enroll(self, get_other_course_settings_mock):
         """Test"""
 
-        get_other_course_settings_mock.return_value = {"domains_allowed": ["test.com"]}
+        get_other_course_settings_mock.return_value = {"filter_enrollment_by_domain_list": ["test.com"]}
 
         with self.assertRaises(CourseEnrollmentStarted.PreventEnrollment):
             FilterEnrollmentByDomain.run_filter(self, self.user, self.course_key, self.mode)
