0d58a6dd24f1dbf50089392133dfc066bf43bb96
Ivo Branco <ivo.branco@fccn.pt>
2022-10-31 14:03:33 +0000

    fix: saml registration extension fix deprecated edx platform import error
    FAN-95

--- nau_openedx_extensions/edxapp_wrapper/backends/registration_h_v1.py
+++ nau_openedx_extensions/edxapp_wrapper/backends/registration_h_v1.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 from openedx.core.djangoapps.user_authn.views.registration_form import get_registration_extension_form
 
-from third_party_auth.saml import EdXSAMLIdentityProvider  # pylint: disable=import-error
+from common.djangoapps.third_party_auth.saml import EdXSAMLIdentityProvider  # pylint: disable=import-error
 
 
 def get_edx_saml_identity_provider():
