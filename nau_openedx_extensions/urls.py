032a59bd79b7f78ef219d3ba1915972817efb348
Beto Fandino <beto3971@gmail.com>
2024-07-03 00:20:08 -0400

    feat: upgraded import of re_path in url

--- nau_openedx_extensions/urls.py
+++ nau_openedx_extensions/urls.py
@@ -3,20 +3,20 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path
 
 from nau_openedx_extensions.message_gateway.views import api as message_gateway_api
 from nau_openedx_extensions.message_gateway.views import tab as message_gateway_tab
 
 urlpatterns = [
-    url(
+    re_path(
         r"^nau-tools/{}/$".format(
             settings.COURSE_ID_PATTERN,
         ),
         message_gateway_tab.NauMessageGatewayTabView.as_view(),
         name="nau_tools",
     ),
-    url(
+    re_path(
         r"^nau-tools/{}/send-message$".format(
             settings.COURSE_ID_PATTERN,
         ),
