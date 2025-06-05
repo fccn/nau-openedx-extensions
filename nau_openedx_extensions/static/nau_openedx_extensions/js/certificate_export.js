cfbb9551a59640691a3dea72490de51280a33e71
efortish <kevynsss@gmail.com>
2025-06-05 13:53:26 -0500

    fix: js url tested

--- nau_openedx_extensions/static/nau_openedx_extensions/js/certificate_export.js
+++ nau_openedx_extensions/static/nau_openedx_extensions/js/certificate_export.js
@@ -10,13 +10,31 @@ if (button) {
             return;
         }
 
-        // Construye la URL según la definición en urls.py
-        const endpoint = `/courses/${courseId}/certificate_export/csv`;
+        // Construye la URL correctamente con el prefijo nau-openedx-extensions
+        const endpoint = `/nau-openedx-extensions/certificate_export/courses/${courseId}/certificate_export/csv`;
+
+        function getCookie(name) {
+            let cookieValue = null;
+            if (document.cookie && document.cookie !== "") {
+                const cookies = document.cookie.split(";");
+                for (let i = 0; i < cookies.length; i++) {
+                    const cookie = cookies[i].trim();
+                    if (cookie.startsWith(name + "=")) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+
+        const csrftoken = getCookie("csrftoken");
 
         fetch(endpoint, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "X-CSRFToken": csrftoken, // Agrega el token CSRF
             },
             body: JSON.stringify({
                 course_id: courseId, // Pasa el course_id en el cuerpo de la solicitud
