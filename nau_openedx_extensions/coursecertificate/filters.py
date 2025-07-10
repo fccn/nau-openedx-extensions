258b58ff5a91f48033376f0b9dbd1f32744f7e54
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-07-10 16:07:58 -0500

    refactor: update certificate filtering by organization to use Django ORM with regex

--- nau_openedx_extensions/coursecertificate/filters.py
+++ nau_openedx_extensions/coursecertificate/filters.py
@@ -9,5 +9,16 @@ def certificate_by_course_id_regex(certificates: QuerySet, course_id_regex: str)
     return certificates.filter(course_id__regex=course_id_regex)
 
 
-def certificate_by_org(certificates: QuerySet, org: str) -> list:
-    return [certificate for certificate in certificates if certificate.course_id.org == org]
+def certificate_by_org(certificates: QuerySet, org: str) -> QuerySet:
+    """
+    Filter certificates by organization using Django ORM.
+
+    Args:
+        certificates (QuerySet): QuerySet of certificates to filter
+        org (str): Organization name to filter by
+
+    Returns:
+        QuerySet: Filtered certificates for the specified organization
+    """
+    org_regex = rf"^course-v1:{org}\+"
+    return certificates.filter(course_id__regex=org_regex)
