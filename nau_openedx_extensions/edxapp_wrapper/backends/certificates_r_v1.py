1bab9c0c8f46b5dad40a1b04d3c1194e84733680
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-08-13 12:24:25 -0500

    feat: add wrapper function to retrieve user certificates

--- nau_openedx_extensions/edxapp_wrapper/backends/certificates_r_v1.py
+++ nau_openedx_extensions/edxapp_wrapper/backends/certificates_r_v1.py
@@ -4,4 +4,8 @@ Real implementation of the certificate backend.
 
 # pylint: disable=import-error, unused-import
 from lms.djangoapps.certificates.models import CertificateHtmlViewConfiguration, GeneratedCertificate
-from lms.djangoapps.certificates.views.webview import _get_catalog_data_for_course, _get_custom_template_and_language
+from lms.djangoapps.certificates.views.webview import (
+    _get_catalog_data_for_course,
+    _get_custom_template_and_language,
+    _get_user_certificate,
+)
