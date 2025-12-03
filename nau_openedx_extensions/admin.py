544e8be1c5294cd6c33c0fb05d0474c30f22cc5b
Tiago-Salles <tiagosalles20@gmail.com>
2025-12-03 15:56:50 +0000

    feat: added partner client to admin
    To enable partner client integrations management
    via admin it was created the admin registration.
    
    related to: https://github.com/fccn/nau-technical/issues/674

--- nau_openedx_extensions/admin.py
+++ nau_openedx_extensions/admin.py
@@ -4,4 +4,5 @@
 from __future__ import absolute_import, unicode_literals
 
 from nau_openedx_extensions.custom_registration_form.admin import *  # pylint: disable=wildcard-import
+from nau_openedx_extensions.partner_integration.admin import *  # pylint: disable=wildcard-import
 from nau_openedx_extensions.student.admin import *  # pylint: disable=wildcard-import
